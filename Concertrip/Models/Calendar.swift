dae30848a549a88a7c420c9d5fb53eadadf27eed
eojin <eozinee@gmail.com>
2019-01-06 15:18:23 +0900

    Add calendarView

---
+++ Concertrip/Models/Calendar.swift
@@ -0,0 +1,36 @@
+//
+//  Calendar.swift
+//  Concertrip
+//
+//  Created by 양어진 on 06/01/2019.
+//  Copyright © 2019 양어진. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+struct Calendar : Mappable {
+    var calendarId : String?
+    var calendarTabId : String?
+    var calendarName : String?
+    var calendarProfileImg : String?
+    var calendarDate : [String]?
+    var calendarTag : String?
+    var calendarSubscribe : Bool?
+    
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        
+        calendarId <- map["_id"]
+        calendarTabId <- map["tabId"]
+        calendarName <- map["name"]
+        calendarProfileImg <- map["profileImg"]
+        calendarDate <- map["date"]
+        calendarTag <- map["tag"]
+        calendarSubscribe <- map["subscribe"]
+    }
+    
+}
